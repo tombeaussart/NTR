b3fb4f824cf6ddb70896f92414df25dd01a1b4c8
Wugop <74765014+Wugop@users.noreply.github.com>
2022-02-21 15:22:55 +0100

    init bank project

---
+++ Rest/src/test/java/fr/insa/bank/BankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
